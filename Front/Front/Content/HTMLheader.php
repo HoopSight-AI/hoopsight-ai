4fe9a534d8365e99dc951a3e57fb459cf0bcac51
Alex Adams <151255913+xXalexAdamsXx@users.noreply.github.com>
2024-10-26 16:18:14 -0700

    Add files via upload

---
+++ Front/Front/Content/HTMLheader.php
@@ -0,0 +1,23 @@
+<?php
+//HTMLheader.php
+?>
+
+<nav id="nav-bar" class="nav">
+	<a class="home-link" href='index.php'>
+		<img class="nav-logo" src='Assets/ball-icon.png' alt='HoopSight AI logo' />
+	</a>
+
+	<ul class="nav-text">
+		<li><a href='index'>HOME</a></li>
+		<li><a href='backDoor.php'>Back door</a></li>
+	</ul>
+
+	<a class="account-button" href='index.php'>
+		<img class="profile-picture" src='Assets/Empty-Profile.png' alt='Log-in image' />
+		<span>Log-In</span>
+	</a>
+</nav>
+
+<!-- The styling for the header is currently dynamically set to whatever the height of the nav-bar is styled as. This will ensure that the rest of the page will show -->
+<div id='top-spacing'></div>
+<script src="JS/muscle.js"></script>
