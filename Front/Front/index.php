371adda1dbc8d64ce1f1c3ee39f912b8649c23cd
Aditya Rao <aditya.k.rao@sjsu.edu>
2024-10-29 13:14:37 -0700

    Update index.php

--- Front/Front/index.php
+++ Front/Front/index.php
@@ -1,17 +1,12 @@
 <?php
-include_once "Content/HTMLhead.php";
-include_once "Content/HTMLheader.php";
-
+include_once dirname(__FILE__) . "/Content/HTMLhead.php";
+include_once dirname(__FILE__) . "/Content/HTMLheader.php";
 echo "<span>Hello. Welcome to the development version of the HoopSight AI websight!</span>";
 ?>
-
 <ul>
-	<h3>TODO:</h3>
-	<li>Get a basic outline of what the inerface should look like</li>
-	<li>Website map</li>
-	<li>Decide on a color scheme</li>
+    <h3>TODO:</h3>
+    <li>Get a basic outline of what the interface should look like</li>
+    <li>Website map</li>
+    <li>Decide on a color scheme</li>
 </ul>
-
-
-
-<?php include_once "Content/HTMLfooter.php"; ?>
+<?php include_once dirname(__FILE__) . "/Content/HTMLfooter.php"; ?>
