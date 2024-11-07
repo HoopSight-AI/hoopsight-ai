4c2effa7279a88841052126ccc30bbb6232c3ed6
Alex Adams <151255913+xXalexAdamsXx@users.noreply.github.com>
2024-11-07 09:39:45 -0800

    Latest front end version
    This version has a basic, functional pop-up for signing up. It will be turned into a sign-in pop-up, and a new page will be created so people can creat accounts.

---
+++ Front/index.php
@@ -0,0 +1,21 @@
+<?php
+include_once "Content/HTMLhead.php";
+include_once "Content/HTMLheader.php";
+include_once "Sign_Up2.php";
+
+
+echo "<span>Hello. Welcome to the development version of the HoopSight AI websight!</span>";
+?>
+
+<ul>
+	<h3>TODO:</h3>
+	<li>Website map</li>
+	<li>Get a basic outline of what the inerface should look like</li>
+	<li>Decide on a color scheme</li>
+</ul>
+
+
+
+
+
+<?php include_once "Content/HTMLfooter.php"; ?>
