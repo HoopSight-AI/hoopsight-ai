94458d9cb9764cf565d40f52f6592d19f7939bd5
Alex Adams <151255913+xXalexAdamsXx@users.noreply.github.com>
2024-11-07 16:28:05 -0800

    Updated Front End
    Responsive sign-up form + additions to landing page

---
+++ Front/AboutUs.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="CSS/AboutUsStyle.css">
+
+<div class='team-portrait-container'>
+	<img src="Assets/HoopSightTeam.png" alt="Photo of the HoopSight AI team. (Just imagine a bunch of really cool people) ">
+</div>
