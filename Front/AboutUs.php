90f485e5dbcbe4413dd99b01da260b3591ea0f05
Alex Adams <151255913+xXalexAdamsXx@users.noreply.github.com>
2024-11-11 18:37:21 -0800

    Delete Front directory

--- Front/AboutUs.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" href="CSS/AboutUsStyle.css">
-
-<div class='team-portrait-container'>
-	<img src="Assets/HoopSightTeam.png" alt="Photo of the HoopSight AI team. (Just imagine a bunch of really cool people) ">
-</div>
