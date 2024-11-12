4b7ebf932671e2475f09d3e389f537b571ac000b
Alex Adams <151255913+xXalexAdamsXx@users.noreply.github.com>
2024-11-11 18:38:36 -0800

    0.1 version of About Us Page
    Beginning development on a page that can introduce the team with a picture and short bio.
    This current version is extremely basic and is by no means working or finished

---
+++ Front/index.php
@@ -0,0 +1,20 @@
+<?php
+include_once "Content/HTMLhead.php";
+include_once "Content/HTMLheader.php";
+
+
+echo "<span>Hello. Welcome to the development version of the HoopSight AI websight!</span>";
+?>
+
+<?php include "AboutUs.php"; ?>
+
+<ul>
+	<h3>TODO:</h3>
+	<li>Website map</li>
+	<li>Get a basic outline of what the inerface should look like</li>
+	<li>Decide on a color scheme</li>
+</ul>
+
+
+
+<?php include_once "Content/HTMLfooter.php"; ?>
