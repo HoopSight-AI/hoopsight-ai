6294d6645da540623c80e81bc4f2c60839f64529
Alex Adams <151255913+xXalexAdamsXx@users.noreply.github.com>
2024-11-11 20:03:31 -0800

    Delete Front directory

--- Front/JS/AboutUsMuscle.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("scroll", () => {
-	const scrollPosition = window.scrollY;
-	const background = document.querySelector(".parallax-background");
-
-	// Adjust parallax speed by modifying the multiplier
-	background.style.transform = `translateY(${scrollPosition * 0.3}px)`;
-});
